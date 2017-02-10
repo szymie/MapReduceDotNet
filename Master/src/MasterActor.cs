8af1c6e052af291864f2c6d2db941b4d1340c4a8
gemboj <adam.debkowski@student.put.poznan.pl>
2017-02-10 11:37:23 +0100

    cleaned Master after tests2

--- Master/src/MasterActor.cs
+++ Master/src/MasterActor.cs
@@ -5,28 +5,11 @@ using System.Collections.Generic;
 
 namespace Master
 {
-	public class TestActor : TypedActor, IHandle<RegisterMapWorkerMessage>, IHandle<Terminated>
+	public class MasterActor : TypedActor, IHandle<RegisterMapWorkerMessage>, IHandle<Terminated>
 	{
-		List<IActorRef> registeredWorkers = new List<IActorRef>();
 
 		public void Handle (RegisterMapWorkerMessage message)
 		{
-			if (registeredWorkers.Count != 0) {
-				var actorRef = registeredWorkers [0];
-			
-				try {
-					//actorRef.Tell("costam");
-					//Context.Stop();
-					actorRef.Tell(Kill.Instance);
-					Console.WriteLine("Killed actor");
-
-				} catch (Exception e) {
-					Console.WriteLine ("Exception while sending.");
-				}
-			}
-
-			Console.WriteLine ("Registered actor: {0}", Sender.Path);
-			registeredWorkers.Add(Sender);
 			Context.Watch (Sender);
 		}
 
@@ -34,26 +17,11 @@ namespace Master
 		{
 			string disconnectedActorPath = message.ActorRef.Path.ToString();
 
-			//Console.WriteLine ("Connection terminated: {0}", message.ToString());
-			int disconnectedActorIndex = registeredWorkers.FindIndex(
-				actorRef => actorRef.Path.ToString().Equals(disconnectedActorPath)
-			);
-
-			if (disconnectedActorIndex != -1) {
-				registeredWorkers.RemoveAt (disconnectedActorIndex);
-				Console.WriteLine ("Deregistered actor: {0}", disconnectedActorPath);
-			} else {
-				Console.WriteLine ("Actor not found: {0}", disconnectedActorPath);
-			}
-
-
-
 		}
 			
-		public TestActor ()
+		public MasterActor ()
 		{			
-			//Receive<TestMessage>(message =>);
-			Console.WriteLine("Waiting for messages...");
+
 		}
 	}
 }
