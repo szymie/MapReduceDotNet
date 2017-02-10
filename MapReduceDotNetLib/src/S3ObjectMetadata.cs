18ebccac55ea07554b9e5815f9a18f430cc54851
Szymon Miężał <szymon.miezal@gmail.com>
2017-02-10 11:52:43 +0100

    S3 lib.

---
+++ MapReduceDotNetLib/src/S3ObjectMetadata.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace MapReduceDotNetLib
+{
+	public class S3ObjectMetadata
+	{
+		public string BucketName { get; private set; }
+		public string Filename { get; private set; }
+
+		public S3ObjectMetadata(string bucketName, string filename)
+		{
+			BucketName = bucketName;
+			Filename = filename;
+		}
+
+		public void remove()
+		{
+
+		}
+
+		public void upStream(Stream stream)
+		{
+
+		}
+
+		public Stream downStream()
+		{
+			return null;
+		}
+}
